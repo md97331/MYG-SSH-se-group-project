1773473ed5734f9053b5a13422a456b4ceb25a2f
gfsalgado1 <giznizzle@gmail.com>
2024-12-04 13:28:47 +0000

    Giselles Flask set up

---
+++ client/src/App.js
@@ -0,0 +1,25 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+function App() {
+    const [message, setMessage] = useState('');
+
+    useEffect(() => {
+        axios.get('http://localhost:5000/api/message')
+            .then((response) => {
+                setMessage(response.data.message);
+            })
+            .catch((error) => {
+                console.error('Error fetching message:', error);
+            });
+    }, []);
+
+    return (
+        <div style={{ textAlign: 'center', marginTop: '50px' }}>
+            <h1>React with Flask Example</h1>
+            <p>{message || 'Loading...'}</p>
+        </div>
+    );
+}
+
+export default App;
