b9f7b06831948bb2c135e61158e8936cbdee4995
Yara824 <abotarayara2003@gmail.com>
2024-12-06 17:22:58 +0000

    update cart tests

--- client/src/tests/cart.test.js
+++ client/src/tests/cart.test.js
@@ -1,6 +1,7 @@
 import { render, screen, fireEvent } from "@testing-library/react";
 import '@testing-library/jest-dom'; 
 import Cart from "../components/Cart/cart"; 
+import App from "../components/HomePage/HomePage";
 
 describe("Shopping Cart App", () => {
     // Mock `window.alert`
@@ -35,27 +36,6 @@ describe("Shopping Cart App", () => {
         // Switch back to Individual Tab
         expect(screen.getByText("Individual Cart")).toBeInTheDocument();
     });
-
-    test("checks out from the individual cart", () => {
-        render(<Cart />);
-        const checkoutButton = screen.getByText("Checkout");
-        fireEvent.click(checkoutButton);
-
-        expect(window.alert).toHaveBeenCalledWith("Proceeding to checkout!");
-    });
-
-    // test("renders shared cart and prevents item removal", () => {
-
-    //     render(<Cart />);
-        
-    //     fireEvent.click(screen.getByText('Group Order'));
-        
-    //     const removeButton = screen.getAllByTestId("Remove-Button")
-        
-    //     fireEvent.click(removeButton);
-    
-    //     expect(window.alert).toHaveBeenCalledWith("Cannot remove items from the shared cart in this demo");
-    // });
     
 });
 
